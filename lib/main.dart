88f9372c010c5fa7e70f3af855e58597ab828d98
Jibin BrandOptics <jibin@brandoptics.com>
2025-06-06 18:15:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:open_ai_pr_review_app_one/sampleclass.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  Widget build(final BuildContext context) =>
+    MaterialApp(
+      title: "Flutter Demo",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "Flutter Demo Home Page"),
     );
-  }
+  
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
@@ -40,8 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(final BuildContext context) =>
+     Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
@@ -51,49 +48,19 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              "You have pushed the button this many times:",
             ),
             Text(
-              '$_counter',
+              "$_counter",
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _incrementCounter();
-        
-          SampleClass sample = SampleClass();   
-
-
-          // Assuming SampleClass has a method called sampleMethod    
-
-          sample.sample
-Method(); // Call the method from SampleClass 
-          sample.sampleMethod(); // Call the method from SampleClass
-          print('SampleClass method called');
-          // You can also use the sample object to access other properties or methods 
-
-          // if needed. 
-          // For example, you can access sample.someProperty if it exists.
-          // This is just an example of how to use the SampleClass in your app.
-          // Make sure to import the SampleClass file at the top of this file.
-
-
-              int a;
-              int b;
-
-              a = 5;
-              b = 10; 
-              int sum = a + b; // This will calculate the sum of a and b
-              print('The sum of $a and $b is $sum'); // This will print the sum to the console
-              // You can use this sum variable in your app as needed.
-          
-        },
-        tooltip: 'Increment',
+        onPressed: ,
+        tooltip: "Increment",
         child: const Icon(Icons.add),
       ),
     );
   }
-}
