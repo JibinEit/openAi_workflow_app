b8b23421d822dbd5696c1c77c8303a40c36af0f4
Jibin BrandOptics <jibin@brandoptics.com>
2025-06-10 10:04:28 +0530

    Update sample_class.dart

--- lib/sample_class.dart
+++ lib/sample_class.dart
@@ -1,8 +1,8 @@
 class Something {
-   Something(this.name, this.value);
+  Something(this.name, this.value, {this.description});
   final String name;
   final int value;
   final String? description;
   @override
   String toString() => "Something(name: $name, value: $value)";
-}
+} 
