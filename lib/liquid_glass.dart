0a69f9b8b084f9036668fe4d026bb9daa702ace6
Jibin BrandOptics <jibin@brandoptics.com>
2025-06-12 13:01:39 +0530

    Update liquid_glass.dart

--- lib/liquid_glass.dart
+++ lib/liquid_glass.dart
@@ -1,5 +1,5 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
+import "dart:ui";
+import "package:flutter/material.dart";
 
 class LiquidGlassWidget extends StatelessWidget {
   const LiquidGlassWidget({
@@ -14,81 +14,79 @@ class LiquidGlassWidget extends StatelessWidget {
   final Widget? child;
 
   @override
-  Widget build(final BuildContext context) {
-    return Center(
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 30,
-              offset: const Offset(0, 10),
-            ),
-            BoxShadow(
-              color: Colors.white.withOpacity(0.05),
-              blurRadius: 10,
-              spreadRadius: 1,
-              offset: const Offset(-5, -5),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Stack(
-            children: [
-              // The blur effect
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-                child: Container(
-                  color: Colors.white.withOpacity(0.1),
-                ),
+  Widget build(final BuildContext context) => Center(
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black.withAlpha((0.2 * 255).toInt()),
+                blurRadius: 30,
+                offset: const Offset(0, 10),
               ),
-              // Glass gradient overlay (simulates light reflection)
-              Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.white.withOpacity(0.4),
-                      Colors.white.withOpacity(0.05),
-                    ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: Colors.white.withOpacity(0.3),
-                    width: 1.5,
+              BoxShadow(
+                color: Colors.white.withAlpha((0.05 * 255).toInt()),
+                blurRadius: 10,
+                spreadRadius: 1,
+                offset: const Offset(-5, -5),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Stack(
+              children: <Widget>[
+                // The blur effect
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+                  child: Container(
+                    color: Colors.white.withAlpha((0.1 * 255).toInt()),
                   ),
                 ),
-              ),
-              // Light reflection shine
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: height * 0.4,
+                // Glass gradient overlay (simulates light reflection)
+                Container(
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
-                      colors: [
-                        Colors.white.withOpacity(0.35),
-                        Colors.transparent,
+                      colors: <Color>[
+                        Colors.white.withAlpha((0.4 * 255).toInt()),
+                        Colors.white.withAlpha((0.05 * 255).toInt()),
                       ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: Colors.white.withAlpha((0.3 * 255).toInt()),
+                      width: 1.5,
                     ),
                   ),
                 ),
-              ),
-              // Child content
-              Center(child: child),
-            ],
+                // Light reflection shine
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: height * 0.4,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Colors.white.withAlpha((0.35 * 255).toInt()),
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                ),
+                // Child content
+                Center(child: child),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
